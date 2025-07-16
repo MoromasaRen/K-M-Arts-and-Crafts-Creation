0005b62143368883724dc021aeed10cee64277b9
MoromasaRen <21104187@usc.edu.ph>
2025-07-16 15:19:43 +0800

    added

--- frontend/admin/Inventory.php
+++ frontend/admin/Inventory.php
@@ -54,7 +54,7 @@ $products = fetchProducts($searchTerm, $statusFilter, $sortOrder, $limit, $offse
       <div class="pl-1 py-1 px-2 rounded text-[#0f2e4d]">Menu</div>
       <a class="pl-4 py-1 px-2 rounded hover:bg-blue-100" href="Dashboard.html">Dashboard</a>
       <a class="pl-4 py-1 px-2 rounded hover:bg-blue-100" href="Order.php">Orders</a>
-      <a class="pl-4 py-1 px-2 rounded hover:bg-blue-100" href="Delivery.html">Deliveries</a>
+      <a class="pl-4 py-1 px-2 rounded hover:bg-blue-100" href="Delivery.php">Deliveries</a>
       <a class="pl-4 py-1 px-2 rounded bg-blue-100" href="Inventory.php">Inventory</a>
       <a class="mt-6 py-1 px-2 rounded hover:bg-blue-100" href="Profile.html">Profile</a>
     </nav>
